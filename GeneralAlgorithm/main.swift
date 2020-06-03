843639bf2d644c6fe58115f994e4f04606d9a772
subash <thapa.subash345@gmail.com>
2020-06-03 05:05:19 -0700

    Initial Commit

---
+++ GeneralAlgorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GeneralAlgorithm
+//
+//  Created by subash on 6/3/20.
+//  Copyright © 2020 symbolicTrace. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
